cbb89a266324d81b03d84c9ec3a2375eb0f11cf2
Nutella <miguelsfimw@gmail.com>
2026-08-22 13:46:27 -0300

    Update Weapons icon asset ID

--- UI.lua
+++ UI.lua
@@ -10,7 +10,7 @@ local Elastic = loadstring(game:HttpGet(
 local Icons = {
 	Combat  = "rbxassetid://10734950020",
 	Visuals = "rbxassetid://10709790948",
-	Weapons = "rbxassetid://9094483852",
+	Weapons = "rbxassetid://9094483835",
 	Sounds  = "rbxassetid://10734896206",
 	Tracers = "rbxassetid://94654949230438",
 }
